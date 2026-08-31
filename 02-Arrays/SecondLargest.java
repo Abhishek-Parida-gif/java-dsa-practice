bddd91a415964024cf887c1095649b4291f245d2
Abhishek Parida <paridaabhishek873@gmail.com>
2026-08-31 16:55:18 +0530

    Implement SecondLargest program to find second largest
    This program finds the second largest element in an array of integers, ensuring there are at least two distinct elements.

---
+++ 02-Arrays/SecondLargest.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        if (n < 2) {
+            System.out.println("Array must contain at least two elements.");
+            sc.close();
+            return;
+        }
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("There is no distinct second largest element.");
+        } else {
+            System.out.println("Largest element = " + largest);
+            System.out.println("Second largest element = " + secondLargest);
+        }
+    }
+}
