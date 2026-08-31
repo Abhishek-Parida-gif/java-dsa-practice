80f4ca9ab8025303964c0e4390923b3412fb7dc7
Abhishek Parida <paridaabhishek873@gmail.com>
2026-08-31 17:12:10 +0530

    Add PalindromeString class to check palindromes

---
+++ 03-Strings/PalindromeString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        if (str.equalsIgnoreCase(reverse)) {
+            System.out.println(str + " is a Palindrome String");
+        } else {
+            System.out.println(str + " is not a Palindrome String");
+        }
+    }
+}
