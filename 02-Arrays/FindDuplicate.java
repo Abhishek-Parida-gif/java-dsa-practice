07374a4fb03f1bb575182febd5b5782907c62048
Abhishek Parida <paridaabhishek873@gmail.com>
2026-08-31 16:57:48 +0530

    Add FindDuplicate program to identify duplicates
    This program prompts the user to enter a number of elements and identifies any duplicate values in the input array.

---
+++ 02-Arrays/FindDuplicate.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class FindDuplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        boolean found = false;
+        System.out.println("Duplicate elements:");
+        for (int i = 0; i < n; i++) {
+            boolean alreadyPrinted = false;
+            for (int k = 0; k < i; k++) {
+                if (arr[i] == arr[k]) {
+                    alreadyPrinted = true;
+                    break;
+                }
+            }
+            if (alreadyPrinted) {
+                continue;
+            }
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.print(arr[i] + " ");
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found) {
+            System.out.print("No duplicate elements");
+        }
+    }
+}
